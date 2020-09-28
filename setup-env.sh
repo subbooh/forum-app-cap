d76f645d5cb2a5a624d51756338f18462d814e5d
I516430 <shivesh.navin@sap.com>
2020-09-28 22:32:01 +0530

    update setup script

--- setup-env.sh
+++ setup-env.sh
@@ -14,13 +14,14 @@ apk add p7zip
 npm i -g @sap/cds-dk --unsafe
 
 # Install java
-mkdir java
-cd java
-wget -O java.tar.gz https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.8/sapmachine-jdk-11.0.8_linux-x64_bin.tar.gz
-7z x java.tar.gz
-7z x java.tar
-cd /root/java/sapmachine-jdk-11.0.8/bin
-chmod 777 *
-export JAVA_HOME=/root/java/sapmachine-jdk-11.0.8/
-export PATH=$PATH:/root/java/sapmachine-jdk-11.0.8/bin
-cd /root
+apk add openjdk11 maven
+# mkdir java
+# cd java
+# wget -O java.tar.gz https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.8/sapmachine-jdk-11.0.8_linux-x64_bin.tar.gz
+# 7z x java.tar.gz
+# 7z x java.tar
+# cd /root/java/sapmachine-jdk-11.0.8/bin
+# chmod 777 *
+# export JAVA_HOME=/root/java/sapmachine-jdk-11.0.8/
+# export PATH=$PATH:/root/java/sapmachine-jdk-11.0.8/bin
+# cd /root
