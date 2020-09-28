37ef05a625acf2a4b0db1943c4387fbb16af8fcc
I516430 <shivesh.navin@sap.com>
2020-09-28 23:16:37 +0530

    Update dockerfile

--- setup-env.sh
+++ setup-env.sh
@@ -10,9 +10,11 @@ apk add p7zip
 #node --harmony index.js -p 8080 -d /
 # cd ~/home
 
+echo 'Installing CDS sdk : This may take a while...'
 # Install cds
 npm i -g @sap/cds-dk --unsafe
 
+echo 'Installing Java'
 # Install java
 apk add openjdk11 maven
 # mkdir java
