1b1e0fbbedc7d59fc93a8caba8ce255883d213d3
I516430 <shivesh.navin@sap.com>
2020-09-28 21:18:00 +0530

    Added setup-env.sh

---
+++ setup-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+apk add --update nodejs npm
+
+# git clone https://github.com/efeiefei/node-file-manager.git
+# cd node-file-manager
+# npm i
+# cd lib
+#node --harmony index.js -p 8080 -d /
+# cd ~/home
+
+npm i -g @sap/cds-dk
